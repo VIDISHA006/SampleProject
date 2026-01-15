8cdfc3e423156dec7f8525fe2c8d038b729f7a84
Vidisha Gupta <144530620+VIDISHA006@users.noreply.github.com>
2026-01-15 19:43:31 +0530

    Add files via upload

---
+++ wwdcApp.swift
@@ -0,0 +1,17 @@
+//
+//  wwdcApp.swift
+//  wwdc
+//
+//  Created by user@48 on 14/01/26.
+//
+
+import SwiftUI
+
+@main
+struct wwdcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
